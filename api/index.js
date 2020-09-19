07aa2975dcb4b9a71e7e3b37b8c484ba9c2783e5
Nicola Marco Decandia <hermedia@Nicolas-MBP.homenet.telecomitalia.it>
2020-09-19 12:55:34 +0200

    update api server

--- api/index.js
+++ api/index.js
@@ -10,12 +10,38 @@ const pool = new Pool({
     connectionString: process.env.CONNECTION_STRING
 });
 
+app.enable('trust proxy');
+
+app.get("/", function(req, res) {
+    
+    var os = require( 'os' );
+    var networkInterfaces = os.networkInterfaces( );
+    
+
+    // var to store the json response
+    var jsonRes = {
+        ContainerIP: networkInterfaces,
+        ContainerHostname : os.hostname()
+        XForwardedfor: req.headers['x-forwarded-for'],
+        RemoteAddress: req.connection.remoteAddress,
+        RemoteHost: req.headers['host'],
+        language: req.headers["accept-language"].split(",")[0],
+        software: req.headers["user-agent"].match(/\(([^)]+)\)/)[1],
+    };
+    
+    // return the response in json format
+    res.json(jsonRes);
+
+})
+
+
 app.get('/data', function(req, res) {
     pool.query('SELECT country, capital from country_and_capitals', [], (err, result) => {
         if (err) {
             return res.status(405).jsonp({
                 error: err
-            });
+            })
+       
         }
 
         return res.status(200).jsonp({
@@ -25,4 +51,4 @@ app.get('/data', function(req, res) {
     });
 });
 
-app.listen(port, () => console.log(`Backend rest api listening on port ${port}!`))
+app.listen(port, () => console.log(`Desotech API Backend os listening on port ${port}!`))
