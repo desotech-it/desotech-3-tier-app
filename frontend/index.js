9403a9c9fd80ada42f51f4a8af0f03ff1d0ca58c
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 20:26:48 +0200

    frontend miglioramenti

--- frontend/index.js
+++ frontend/index.js
@@ -6,31 +6,26 @@ const port = 3000;
 const restApiUrl = process.env.API_URL;
 
 app.get('/', function(req, res) {
-    request(
-        restApiUrl, {
-            method: "GET",
-        },
-        function(err, resp, body) {
-            if (!err && resp.statusCode === 200) {
-                console.log(`Frontend Running Successfully`);
-                var objData = JSON.parse(body);
-                var c_cap = objData.data;
-                var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo" ></br><table border="1"><tr><td>Country</td><td>Capital</td></tr>`;
+    request(restApiUrl, { method: "GET" }, function(err, resp, body) {
+        if (!err && resp.statusCode === 200) {
+            console.log(`Frontend Running Successfully`);
+            var objData = JSON.parse(body);
+            var c_cap = objData.data;
+            var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"></br><table border="1"><tr><td>Country</td><td>Capital</td></tr>`;
 
-                for (var i = 0; i < c_cap.length; i++)
-                    responseString = responseString +
-                      `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
+            for (var i = 0; i < c_cap.length; i++)
+                responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
 
-                responseString = responseString + `</table></body></html>`;
-                res.send(responseString);
-            } else {
-                console.log(err);
-                console.error(err);
-                var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo" ></br>`;
-                responseString = responseString + `<h1>API Server not available</h1></body></html>`;
-                res.send(responseString);
-            }
-        });
+            responseString += `</table></body></html>`;
+            res.send(responseString);
+        } else {
+            console.log(err);
+            console.error(err);
+            var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"></br>`;
+            responseString += `<h1>API Server not available</h1></body></html>`;
+            res.send(responseString);
+        }
+    });
 });
 
-app.listen(port, () => console.log(`Frontend app listening on port ${port}!`))
+app.listen(port, () => console.log(`Frontend app listening on port ${port}!`));
