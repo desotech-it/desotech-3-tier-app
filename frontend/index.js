6c6126ffd0462461335893066f4238ba85a8362a
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 23:53:38 +0200

    fix

--- frontend/index.js
+++ frontend/index.js
@@ -62,31 +62,6 @@ app.get('/', function(req, res) {
             res.status(500).send(errorResponse);
         }
     });
-    request({
-        url: `${backendUrl}/data`,
-        method: "GET",
-        timeout: 3000 // Timeout di 3 secondi
-    }, function(err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-            console.log(`Frontend Running Successfully`);
-            var objData = JSON.parse(body);
-            var c_cap = objData.data;
-            var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br><table><tr><th>Country</th><th>Capital</th></tr>`;
-            responseString += `<h1> Connection to Backend successfully.</h1>`;
-            for (var i = 0; i < c_cap.length; i++)
-                responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
-
-            responseString += `</table></body></html>`;
-            res.send(responseString);
-
-        } else {
-            console.log(err);
-            console.error(err);
-            var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-            errorResponse += `<h1>Unable to contact API Server</h1></body></html>`;
-            res.status(500).send(errorResponse);
-        }
-    });
 });
 
 app.listen(port, () => console.log(`Frontend app listening on port ${port}!`));
