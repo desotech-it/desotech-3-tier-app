d60dbe9f99fdf0edbd16fb32e5f69d2100369567
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 20:27:50 +0200

    add build.sh for frontend

---
+++ frontend/build.sh
@@ -0,0 +1,6 @@
+VERSION="1.1.0"
+sudo docker build --no-cache -t r.deso.tech/3tier/frontend:$VERSION .
+sudo docker push r.deso.tech/3tier/frontend:$VERSION
+kubectl delete pod -l app=frontend -n frontend-namespace
+sleep 10
+kubectl logs -n frontend-namespace -l app=frontend -f
