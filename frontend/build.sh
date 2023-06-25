a72d4cff3070df07c8543d8cdec6c0aed2b99ec0
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 21:12:51 +0200

    fix build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,7 +1,8 @@
 git pull
 VERSION="1.1.0"
+kubectl delete deploy -n frontend-namespace frontend-deployment --grace-period 0 --force
 sudo docker build --no-cache -t r.deso.tech/3tier/frontend:$VERSION .
 sudo docker push r.deso.tech/3tier/frontend:$VERSION
-kubectl delete pod -l app=frontend -n frontend-namespace
+kubectl apply -f ../kubernetes/manifest.yaml
 sleep 5
 kubectl logs -n frontend-namespace -l app=frontend -f
