3903f452c64b9590ce6ff783de3bca879223797e
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-26 00:34:03 +0200

    fix

--- frontend/index.js
+++ frontend/index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 const backendUrl = process.env.API_URL;
 
+
 // Aggiunta del CSS per la tabella e il logo
 const css = `
     <style>
@@ -37,6 +38,7 @@ const css = `
     </style>
 `;
 
+
 app.get('/', function(req, res) {
     async.parallel({
         root: function(callback) {
@@ -45,20 +47,17 @@ app.get('/', function(req, res) {
                 method: "GET",
                 timeout: 3000
             }, function(err, resp, body) {
-                if (err && err.code === 'ESOCKETTIMEDOUT') {
+                var rootData = {};
+                if (err) {
                     console.error(err);
-                    var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-                    errorResponse += `<h1>Database Connection Timeout</h1></body></html>`;
-                    res.status(500).send(errorResponse);
-                    return;
-                } else if (err || resp.statusCode !== 200) {
-                    console.error(err || new Error("Bad status code from root API call"));
-                    var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-                    errorResponse += `<h1>Unable to contact API Server</h1></body></html>`;
-                    res.status(500).send(errorResponse);
-                    return;
+                    rootData = { error: "Unable to contact Backend API" };
+                } else if (resp.statusCode !== 200) {
+                    console.error(new Error("Bad status code from root API call"));
+                    rootData = { error: "Bad status code from Backend API" };
+                } else {
+                    rootData = JSON.parse(body);
                 }
-                callback(null, JSON.parse(body));
+                callback(null, rootData);
             });
         },
         data: function(callback) {
@@ -67,61 +66,52 @@ app.get('/', function(req, res) {
                 method: "GET",
                 timeout: 3000
             }, function(err, resp, body) {
-                if (err && err.code === 'ESOCKETTIMEDOUT') {
+                var data = [];
+                if (err) {
                     console.error(err);
-                    var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-                    errorResponse += `<h1>Database Connection Timeout</h1></body></html>`;
-                    res.status(500).send(errorResponse);
-                    return;
-                } else if (err || resp.statusCode !== 200) {
-                    console.error(err || new Error("Bad status code from data API call"));
-                    var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-                    errorResponse += `<h1>Unable to contact API Server</h1></body></html>`;
-                    res.status(500).send(errorResponse);
-                    return;
+                } else if (resp.statusCode !== 200) {
+                    console.error(new Error("Bad status code from data API call"));
+                } else {
+                    data = JSON.parse(body).data;
                 }
-                callback(null, JSON.parse(body));
+                callback(null, data);
             });
         }
     }, function(err, results) {
-        if (err) {
-            console.error(err);
-            var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
-            errorResponse += `<h1>Unable to contact API Server</h1></body></html>`;
-            res.status(500).send(errorResponse);
-            return;
-        }
-
-        // Raccogli i risultati in una variabile
         var rootData = results.root;
-        var c_cap = results.data.data;
+        var c_cap = results.data;
 
-        // Costruisci la stringa di risposta come desideri
         var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
         responseString += `<h1>Connection to Backend successfully.</h1>`;
 
-        // Prima tabella con /data
-        responseString += `<table><tr><th>Country</th><th>Capital</th></tr>`;
-        for (var i = 0; i < c_cap.length; i++)
-            responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
-        responseString += `</table>`;
-
-        // Seconda tabella con /
-        responseString += `<h1>Information of Backend Pod:</h1>`;
-        responseString += `<table><tr><th>Property</th><th>Value</th></tr>`;
-        for (const [key, value] of Object.entries(rootData)) {
-            if (key === 'ContainerIP' && value.eth0) {
-                // Mostra solo l'indirizzo IP dell'interfaccia eth0
-                for (const interface of value.eth0) {
-                    if (interface.family === 'IPv4') {
-                        responseString += `<tr><td>${key}</td><td>${interface.address}</td></tr>`;
+        if (rootData.error) {
+            responseString += `<h2>${rootData.error}</h2>`;
+        } else {
+            // Seconda tabella con /
+            responseString += `<h1>Information of Backend Pod:</h1>`;
+            responseString += `<table><tr><th>Property</th><th>Value</th></tr>`;
+            for (const [key, value] of Object.entries(rootData)) {
+                if (key === 'ContainerIP' && value.eth0) {
+                    // Mostra solo l'indirizzo IP dell'interfaccia eth0
+                    for (const interface of value.eth0) {
+                        if (interface.family === 'IPv4') {
+                            responseString += `<tr><td>${key}</td><td>${interface.address}</td></tr>`;
+                        }
                     }
+                } else {
+                    responseString += `<tr><td>${key}</td><td>${value}</td></tr>`;
                 }
-            } else {
-                responseString += `<tr><td>${key}</td><td>${value}</td></tr>`;
             }
+            responseString += `</table>`;
         }
-        responseString += `</table>`;
+
+        if (c_cap.length > 0) {
+            // Prima tabella con /data
+            responseString += `<table><tr><th>Country</th><th>Capital</th></tr>`;
+            for (var i = 0; i < c_cap.length; i++)
+                responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
+            responseString += `</table>`;
+        }
 
         responseString += `</body></html>`;
         res.send(responseString);
