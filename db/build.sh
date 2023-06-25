b273083c49e837ea269bc6845fd99e8c58a36c3c
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 19:13:11 +0200

    trying fixing db

--- db/build.sh
+++ db/build.sh
@@ -2,3 +2,4 @@ VERSION="1.1.0"
 sudo docker build --no-cache -t r.deso.tech/3tier/db:$VERSION .
 sudo docker push r.deso.tech/3tier/db:$VERSION
 kubectl delete pod -l app=db -n db-namespace
+kubectl logs -n db-namespace -l app=db
