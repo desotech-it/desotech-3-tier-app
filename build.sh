55f76ae2c8d486edc96a430768c503ec006870c5
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 13:13:35 +0200

    fix folder name

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 VERSION="1.1.0"
 
-sudo docker build --no-cache -t r.deso.tech/3tier/frontend:$VERSION .
+sudo docker build --no-cache -t r.deso.tech/3tier/frontend:$VERSION frontend/.
 sudo docker push r.deso.tech/3tier/frontend:$VERSION
 
-sudo docker build --no-cache -t r.deso.tech/3tier/backend:$VERSION .
+sudo docker build --no-cache -t r.deso.tech/3tier/backend:$VERSION backend/.
 sudo docker push r.deso.tech/3tier/backend:$VERSION
 
-sudo docker build --no-cache -t r.deso.tech/3tier/db:$VERSION .
+sudo docker build --no-cache -t r.deso.tech/3tier/db:$VERSION db/.
 sudo docker push r.deso.tech/3tier/db:$VERSION
