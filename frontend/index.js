1c0e51b19ea62a6a2fddfd2862c355ccfbcb5389
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-26 00:08:44 +0200

    fix

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const request = require('request');
+const async = require('async');
 
 const app = express();
 const port = 3000;
 const backendUrl = process.env.API_URL;
+
 
 // Aggiunta del CSS per la tabella e il logo
 const css = `
@@ -36,31 +38,57 @@ const css = `
     </style>
 `;
 
+
 app.get('/', function(req, res) {
-    request({
-        url: `${backendUrl}/data`,
-        method: "GET",
-        timeout: 3000 // Timeout di 3 secondi
-    }, function(err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-            console.log(`Frontend Running Successfully`);
-            var objData = JSON.parse(body);
-            var c_cap = objData.data;
-            var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br><table><tr><th>Country</th><th>Capital</th></tr>`;
-            responseString += `<h1> Connection to Backend successfully.</h1>`;
-            for (var i = 0; i < c_cap.length; i++)
-                responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
-
-            responseString += `</table></body></html>`;
-            res.send(responseString);
-
-        } else {
-            console.log(err);
-            console.error(err);
+    async.parallel({
+        root: function(callback) {
+            request({
+                url: `${backendUrl}/`,
+                method: "GET",
+                timeout: 3000
+            }, function(err, resp, body) {
+                if (err || resp.statusCode !== 200) {
+                    console.error(err || new Error("Bad status code from root API call"));
+                    callback(err);
+                    return;
+                }
+                callback(null, JSON.parse(body));
+            });
+        },
+        data: function(callback) {
+            request({
+                url: `${backendUrl}/data`,
+                method: "GET",
+                timeout: 3000
+            }, function(err, resp, body) {
+                if (err || resp.statusCode !== 200) {
+                    console.error(err || new Error("Bad status code from data API call"));
+                    callback(err);
+                    return;
+                }
+                callback(null, JSON.parse(body));
+            });
+        }
+    }, function(err, results) {
+        if (err) {
             var errorResponse = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
             errorResponse += `<h1>Unable to contact API Server</h1></body></html>`;
             res.status(500).send(errorResponse);
+            return;
         }
+
+        // Raccogli i risultati in una variabile
+        var rootData = results.root;
+        var c_cap = results.data.data;
+
+        // Costruisci la stringa di risposta come desideri, qui l'ho mantenuta molto basilare
+        var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br><table><tr><th>Country</th><th>Capital</th></tr>`;
+        responseString += `<h1> Connection to Backend successfully.</h1>`;
+        responseString += JSON.stringify(rootData);
+        for (var i = 0; i < c_cap.length; i++)
+            responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
+        responseString += `</table></body></html>`;
+        res.send(responseString);
     });
 });
 
