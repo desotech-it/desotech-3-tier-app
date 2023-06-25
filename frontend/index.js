6b9f5fa01655c0f10b181880125cda32291de80f
Nicola Marco Decandia <nicola@decandia.eu>
2023-06-25 20:30:26 +0200

    add css to frontend

--- frontend/index.js
+++ frontend/index.js
@@ -5,13 +5,44 @@ const app = express();
 const port = 3000;
 const restApiUrl = process.env.API_URL;
 
+// Aggiunta del CSS per la tabella e il logo
+const css = `
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        img {
+            margin-bottom: 20px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 400px;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+`;
+
 app.get('/', function(req, res) {
     request(restApiUrl, { method: "GET" }, function(err, resp, body) {
         if (!err && resp.statusCode === 200) {
             console.log(`Frontend Running Successfully`);
             var objData = JSON.parse(body);
             var c_cap = objData.data;
-            var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"></br><table border="1"><tr><td>Country</td><td>Capital</td></tr>`;
+            var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br><table><tr><th>Country</th><th>Capital</th></tr>`;
 
             for (var i = 0; i < c_cap.length; i++)
                 responseString += `<tr><td>${c_cap[i].country}</td><td>${c_cap[i].capital}</td></tr>`;
@@ -21,7 +52,7 @@ app.get('/', function(req, res) {
         } else {
             console.log(err);
             console.error(err);
-            var responseString = `<html><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"></br>`;
+            var responseString = `<html><head>${css}</head><body><img src="https://www.deso.tech/wp-content/uploads/2023/03/desotech-300x133.png" alt="logo"><br>`;
             responseString += `<h1>API Server not available</h1></body></html>`;
             res.send(responseString);
         }
